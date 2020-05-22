799fc8c3b680dc4fd778a5a8d069af3bdd0b63c1
rselent <rselent@ufl.edu>
2020-05-21 20:33:12 -0400

    Update single_number.py

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -4,9 +4,37 @@ Returns: an integer
 '''
 def single_number(arr):
     # Your code here
+    singles = []
 
-    pass
+    for n in arr:
+        if n not in singles:
+            singles.append( n)
+        else:
+            singles.remove( n)
+    return singles[0]
 
+def single_number2(arr):
+    # Your code here
+    counts = {}
+
+    for n in arr:
+        if n in counts:
+            counts[n] += 1
+        else:
+            counts[n] = 1
+    for num in counts:
+        if counts[num] == 1:
+            return num
+
+def single_number3(arr):
+    # Your code here
+    from collections import Counter
+    
+    counts = Counter(arr)
+
+    for n in counts.elements():
+        if counts.get(n) == 1:
+            return n
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
