dd608721e5394dab3656b9389572dbf912208bc5
rselent <rselent@ufl.edu>
2020-05-21 20:33:21 -0400

    Update test_single_number.py

--- single_number/test_single_number.py
+++ single_number/test_single_number.py
@@ -1,6 +1,6 @@
 import unittest
 import random
-from single_number import single_number
+from single_number import single_number, single_number2, single_number3
 
 class Test(unittest.TestCase):
     def test_single_number(self):
@@ -14,7 +14,9 @@ class Test(unittest.TestCase):
         rand_index = random.randint(0, len(arr))
         num = arr.pop(rand_index)
 
-        self.assertEqual(single_number(arr), num)
+#        self.assertEqual(single_number(arr), num)
+#        self.assertEqual(single_number2(arr), num)
+        self.assertEqual(single_number3(arr), num)
 
 
 if __name__ == '__main__':
